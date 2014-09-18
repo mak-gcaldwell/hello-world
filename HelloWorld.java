1eee4fa2849b1eaf788079061ca6d68d82ce2316
Gerald Caldwell <Gerald.Caldwell@MAK-Solutions.com>
2014-09-18 15:01:50 -0400

    Added a third line "This is another new line"

--- HelloWorld.java
+++ HelloWorld.java
@@ -3,5 +3,6 @@ public class HelloWorld {
    public static void main(String[] args) { 
       System.out.println("Hello, World Gerald");
       System.out.println("This is a new line");
+      System.out.println("This is another new line!!!")
    }
 }
